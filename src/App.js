82aaa1f88bbc8afa2edf361bf7fe4e95134a3685
shtekhler <karaestubbs@gmail.com>
2017-10-07 09:20:28 +0100

    Added parallaximg and first article

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import styled from "styled-components";
 import menuicon from "./menuicon.png";
 
 const Wrapper = styled.div`
-  font-family: "Open Sans";
+  font-family: "Roboto", sans-serif;
   color: black;
-  background-color: white;
-  padding: 5px;
 `;
 
 const NavWrapper = styled.nav`
@@ -21,7 +19,7 @@ const NavWrapper = styled.nav`
 
 const HeaderLeft = styled.span`
   align-self: flex-start;
-  margin: 5px;
+  margin: 10px;
   font-weight: bold;
 `;
 
@@ -31,13 +29,72 @@ const MenuButton = styled.button`
   border: none;
   background: rgba(0, 0, 0, 0);
   float: right;
-  margin: 5px;
+  margin: 10px 30px 10px 0px;
   &:hover ${MenuImage} {
     background: black;
   }
 `;
 
-const HeaderWrapper = styled.header`display: flex;`;
+const TopLeftHeaderWrapper = styled.header`display: flex;`;
+
+const SecondHeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  height: 10%;
+  background: white;
+  margin: 80px 10px 60px 10px;
+`;
+
+const HeaderBold = styled.header`
+  flex: 2;
+  font-weight: bold;
+  font-size: 2em;
+`;
+
+const SubHeader = styled.header`
+  flex: 1;
+  color: rgb(218, 218, 218);
+  font-size: 2em;
+`;
+
+const ParallaxImg = styled.div`
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/9/96/Toronto_-_ON_-_Toronto_Harbourfront7.jpg");
+  height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Article1Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+  margin: 80px 10px 60px 10px;
+  justify-content: space-between;
+`;
+
+const Article1Left = styled.section`
+  flex: 1;
+  margin: 10px;
+`;
+
+const HeaderBold1 = styled.header`
+  font-weight: bold;
+  font-size: 1.5em;
+`;
+
+const SubHeader1 = styled.header`
+  color: rgb(218, 218, 218);
+  font-size: 1.5em;
+`;
+
+const Article1Right = styled.section`
+  flex: 1;
+  margin: 0px 10px 0px 10px;
+`;
 
 class App extends Component {
   render() {
@@ -48,16 +105,56 @@ class App extends Component {
             <MenuImage src={menuicon} />
           </MenuButton>
         </NavWrapper>
-        <HeaderWrapper>
+        <TopLeftHeaderWrapper>
           <HeaderLeft>
             <span>Kara.</span>
           </HeaderLeft>
-        </HeaderWrapper>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/96/Toronto_-_ON_-_Toronto_Harbourfront7.jpg"
-          width="100%"
-          alt="CN Tower wikimedia"
-        />
+        </TopLeftHeaderWrapper>
+        <SecondHeaderWrapper>
+          <HeaderBold>Lorem Ipsum Dolor</HeaderBold>
+          <SubHeader>Sed do eiusmod tempor</SubHeader>
+        </SecondHeaderWrapper>
+        <ParallaxImg />
+        <Article1Wrapper>
+          <Article1Left>
+            <HeaderBold1>Incididunt ut labore et dolore magna</HeaderBold1>
+            <SubHeader1>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip
+            </SubHeader1>
+          </Article1Left>
+          <Article1Right>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+            </p>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+            </p>{" "}
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Article1Right>
+        </Article1Wrapper>
       </Wrapper>
     );
   }
